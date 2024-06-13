c86d0b32b19cbe511a82121b676c3c1b79a9e36e
mkurniafebrianto <mkurniafebrianto@gmail.com>
2024-06-13 13:13:58 +0700

    fix the code

--- lib/src/Attendance.java
+++ lib/src/Attendance.java
@@ -2,7 +2,7 @@ import java.time.LocalTime;
 
 public class Attendance {
 
-  public static String checkAttendanceInAndOut(LocalTime time, boolean isCheckIn, boolean isButtonEnabled) {
+  public static String checkAttendanceInAndOut(LocalTime time, boolean isCheckIn) {
 
     // define range of attendance in and out
     final LocalTime startCheckIn = LocalTime.of(6, 0, 0); // 06:00:00
@@ -12,35 +12,58 @@ public class Attendance {
     final LocalTime startCheckOut = LocalTime.of(16, 30, 0); // 16:30:00
     final LocalTime endCheckOut = LocalTime.of(17, 0, 0); // 17:00:00
 
+    // define initial state for button
+    boolean isButtonEnabled = false;
+
+    // if checkin
     if (isCheckIn) {
+
       // check whether checkin time in range or not
       if ((time.isAfter(startCheckIn) || time.equals(startCheckIn))
           && (time.isBefore(maxToleranceCheckIn) || time.equals(maxToleranceCheckIn))) {
-        // if button enabled
-        if (isButtonEnabled) {
-          if ((time.isAfter(startCheckIn) || time.equals(startCheckIn))
-              && (time.isBefore(endCheckIn) || time.equals(endCheckIn))) {
-            return "presensi masuk berhasil dan kamu tepat waktu, MANTAB!";
-          } else if ((time.isAfter(endCheckIn))
-              && (time.isBefore(maxToleranceCheckIn) || time.equals(maxToleranceCheckIn))) {
-            return "presensi masuk berhasil tapi sayang kamu terlambat";
-          }
+
+        // enable button
+        isButtonEnabled = true;
+
+        // check attendance and return str
+        if ((time.isAfter(startCheckIn) || time.equals(startCheckIn))
+            && (time.isBefore(endCheckIn) || time.equals(endCheckIn))) {
+          return "presensi masuk berhasil dan kamu tepat waktu, MANTAB!";
+        } else if ((time.isAfter(endCheckIn))
+            && (time.isBefore(maxToleranceCheckIn) || time.equals(maxToleranceCheckIn))) {
+          return "presensi masuk berhasil tapi sayang kamu terlambat";
         }
+
       } else {
-        // if not in range of checkin time & button disabled
+
+        // disable button
+        isButtonEnabled = false;
+
+        // return str
         return "maaf presensi masuk gagal karena diluar waktu yang ditentukan";
       }
-      
+
+      // if checkout
     } else {
+
       // check whether checkout time in range or not
       if ((time.isAfter(startCheckOut) || time.equals(startCheckOut))
           && (time.isBefore(endCheckOut) || time.equals(endCheckOut))) {
+
+        // disable button
+        isButtonEnabled = true;
+
+        // return str
         return "presensi keluar berhasil, pulang pulang!";
+
       } else {
+
+        // return str
         return "maaf presensi keluar gagal karena diluar waktu yang ditentukan";
       }
     }
 
+    // if there is no condition match, return null
     return null;
   }
 
--- lib/src/TestAttendance.java
+++ lib/src/TestAttendance.java
@@ -20,42 +20,38 @@ public class TestAttendance {
         public boolean isCheckIn;
 
         @Parameterized.Parameter(3)
-        public boolean isButtonEnabled;
-
-        @Parameterized.Parameter(4)
         public String expectedResult;
 
         @Parameters(name = "{index}: {0} - {1} - {2} - {3}")
         public static Collection<Object[]> data() {
                 return Arrays.asList(new Object[][] {
-                                { "Presensi Masuk Tepat di Batas Awal", LocalTime.of(6, 0, 0), true, true,
+                                { "Presensi Masuk Tepat di Batas Awal", LocalTime.of(6, 0, 0), true,
                                                 "presensi masuk berhasil dan kamu tepat waktu, MANTAB!" },
                                 { "Presensi Masuk Dalam Rentang Waktu yang Diperbolehkan", LocalTime.of(8, 0, 0), true,
-                                                true,
                                                 "presensi masuk berhasil dan kamu tepat waktu, MANTAB!" },
-                                { "Presensi Masuk Tepat di Batas Akhir ", LocalTime.of(8, 30, 0), true, true,
+                                { "Presensi Masuk Tepat di Batas Akhir ", LocalTime.of(8, 30, 0), true,
                                                 "presensi masuk berhasil dan kamu tepat waktu, MANTAB!" },
-                                { "Presensi Masuk Tepat Setelah Batas Akhir ", LocalTime.of(8, 30, 1), true, true,
+                                { "Presensi Masuk Tepat Setelah Batas Akhir ", LocalTime.of(8, 30, 1), true,
                                                 "presensi masuk berhasil tapi sayang kamu terlambat" },
-                                { "Presensi Masuk Tepat di Batas Akhir Toleransi", LocalTime.of(9, 0, 0), true, true,
+                                { "Presensi Masuk Tepat di Batas Akhir Toleransi", LocalTime.of(9, 0, 0), true,
                                                 "presensi masuk berhasil tapi sayang kamu terlambat" },
-                                { "Presensi Masuk Di Luar Batas Waktu Toleransi ", LocalTime.of(9, 0, 1), true, false,
+                                { "Presensi Masuk Di Luar Batas Waktu Toleransi ", LocalTime.of(9, 0, 1), true,
                                                 "maaf presensi masuk gagal karena diluar waktu yang ditentukan" },
 
-                                { "Presensi Keluar Tepat di Batas Awal", LocalTime.of(16, 30, 0), false, true,
+                                { "Presensi Keluar Tepat di Batas Awal", LocalTime.of(16, 30, 0), false,
                                                 "presensi keluar berhasil, pulang pulang!" },
                                 { "Presensi Keluar Dalam Rentang Waktu yang Diperbolehkan", LocalTime.of(16, 45, 0),
-                                                false, true,
+                                                false,
                                                 "presensi keluar berhasil, pulang pulang!" },
-                                { "Presensi Keluar Tepat di Batas Akhir ", LocalTime.of(17, 0, 0), false, true,
+                                { "Presensi Keluar Tepat di Batas Akhir ", LocalTime.of(17, 0, 0), false,
                                                 "presensi keluar berhasil, pulang pulang!" },
-                                { "Presensi Keluar Tepat Setelah Batas Akhir ", LocalTime.of(17, 0, 1), false, false,
+                                { "Presensi Keluar Tepat Setelah Batas Akhir ", LocalTime.of(17, 0, 1), false,
                                                 "maaf presensi keluar gagal karena diluar waktu yang ditentukan" }
                 });
         }
 
         @Test
         public void testCheckAttendanceInAndOut() {
-                assertEquals(expectedResult, Attendance.checkAttendanceInAndOut(time, isCheckIn, isButtonEnabled));
+                assertEquals(expectedResult, Attendance.checkAttendanceInAndOut(time, isCheckIn));
         }
 }
